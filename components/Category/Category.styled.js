33d800ec462d635e88cc24a1fbeb82fcc4be61b6
Berzel Best <berzel@classifieds.co.zw>
2021-10-14 16:51:16 +0200

    Increase category styles padding

--- components/Category/Category.styled.js
+++ components/Category/Category.styled.js
@@ -2,7 +2,7 @@ import tw from "twin.macro";
 import styled from "styled-components";
 
 const CategoryStyles = styled.div`
-    ${tw`text-gray-900 rounded-lg bg-gray-200 p-2 whitespace-nowrap`}
+    ${tw`text-gray-900 rounded-lg bg-gray-200 p-3 whitespace-nowrap`}
 `;
 
 export default CategoryStyles
