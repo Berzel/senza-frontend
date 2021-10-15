c914210e37fd0d18074f3c1480857e04b6b2def3
Berzel Best <berzel@classifieds.co.zw>
2021-10-15 16:43:29 +0200

    Add background image on post job banner

--- components/PostJob/PostJobBanner.styled.js
+++ components/PostJob/PostJobBanner.styled.js
@@ -2,9 +2,13 @@ import tw from "twin.macro";
 import styled from "styled-components";
 
 const PostJobBannerStyles = styled.div`
+    background-size: 200%;
     ${tw`px-3 bg-gray-700 p-4 text-white shadow-lg text-center bg-gradient-to-t from-gray-700 to-gray-800`}
+    background-image: linear-gradient(to bottom, rgba(59,130,246,0.95), rgb(31 41 55)), url('/img/circuit.jpg');
 
     @media screen and (min-width: 1280px) {
+        background-size: cover;
+        background-image: linear-gradient(to right, rgba(59,130,246,0.95), rgb(31 41 55)), url('/img/circuit.jpg');
         ${tw`text-left mx-3 rounded-lg flex items-end justify-between`}
     }
 
