b1d904ab0177b342fe327fedd27d43ad9b9ff550
Berzel Best <berzel@classifieds.co.zw>
2021-10-23 19:21:55 +0200

    Infinite scroll

--- components/JobSummary/JobSummaryList.js
+++ components/JobSummary/JobSummaryList.js
@@ -1,16 +1,51 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import JobSummary from "./JobSummary";
 import JobListStyles from "./JobSummaryList.styled";
 
 const JobSummaryList = ({title, jobs}) => {
 
-    const [activeJob, setActiveJob] = useState(jobs?.data?.length > 0 ? jobs.data[0] : null)
+    const [allPages, setAllPages] = useState([jobs]);
+    const [scrolledTo, setScrolledTo] = useState(0);
+    const getAllJobs = () => allPages.reduce((allJobs, el) => allJobs.concat(el.data), []);
+    const [activeJob, setActiveJob] = useState(getAllJobs().length > 0 ? getAllJobs()[0] : null)
+
+    const getNextPage = async () => {
+        const previousPage = allPages[allPages.length - 1];
+        if (!previousPage.next_page_url) return;
+        const nextPage = await axios.get(`${previousPage.next_page_url}&size=${previousPage.per_page}`).then(r => r.data)
+        setAllPages([...allPages, nextPage]);
+    }
 
     useEffect(() => {
-        if (jobs?.data?.length > 0) {
-            setActiveJob(jobs.data[0])
+        if (getAllJobs().length > 0) {
+            setActiveJob(getAllJobs()[0])
         }
+
+        document.addEventListener('scroll', () => {
+            let ticking = false;
+            let lastScrollPos = 0;
+
+            if (!ticking) {
+                setTimeout(() => {
+                    let currentPos = window.scrollY;
+                    if (currentPos > lastScrollPos) setScrolledTo(currentPos)
+                    lastScrollPos =  window.scrollY;
+                    ticking = false;
+                }, 2000)
+            }
+
+            ticking = true;
+        })
     }, [])
+
+    useEffect(() => {
+        getNextPage()
+    }, [scrolledTo])
+
+    const fetchNextJobs = e => {
+        getNextPage()
+    }
 
     return (
         <JobListStyles>
@@ -18,14 +53,14 @@ const JobSummaryList = ({title, jobs}) => {
             <div className="jobs">
                 <ul className="list">
                     {
-                        jobs?.data?.map(job => (
+                        getAllJobs().map(job => (
                             <li key={job.id}>
                                 <JobSummary setActiveJob={setActiveJob} job={job} active={activeJob.id === job.id} />
                             </li>)
                         )
                     }
                     <li>
-                        <button className="more">
+                        <button className="more" onClick={fetchNextJobs}>
                             More
                         </button>
                     </li>
@@ -41,7 +76,7 @@ const JobSummaryList = ({title, jobs}) => {
                     {
                         !activeJob && (
                             <h1>
-                                No job selected
+                                Nothing to show
                             </h1>
                         )
                     }
--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home({sectors, latestJobs}) {
 
 export async function getStaticProps(context) {
   const sectors = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sectors/all`).then(r => r.data)
-  const latestJobs = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/jobs/latest`).then(r => r.data)
+  const latestJobs = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/jobs/latest?size=15`).then(r => r.data)
 
   return {
       props: {
