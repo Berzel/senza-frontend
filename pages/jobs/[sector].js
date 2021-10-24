94596d2fbe78dfa5987ae756bf4e541f8b9adaf9
Berzel Best <berzel@classifieds.co.zw>
2021-10-24 18:35:19 +0200

    Load 1 job per page show test the load more button

--- pages/jobs/[sector].js
+++ pages/jobs/[sector].js
@@ -51,7 +51,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     const sector = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sectors/${params.sector}`).then(r => r.data)
-    sector.latestJobs = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sectors/${params.sector}/jobs`).then(r => r.data)
+    sector.latestJobs = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sectors/${params.sector}/jobs?size=1`).then(r => r.data)
 
     return {
         props: {
