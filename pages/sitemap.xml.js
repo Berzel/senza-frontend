77614f506c2a15492f50bd540cd2db61e54ff129
Berzel Best <berzel@classifieds.co.zw>
2021-10-24 20:21:25 +0200

    Add sitemap to website

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const Sitemap = () => {};
+
+export default Sitemap
+
+export const getServerSideProps = async ({ res }) => {
+    const sectors = await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sitemaps/sectors`).then(r => r.data)
+    let allPages = [await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sitemaps/jobs?size=1000`).then(r => r.data)];
+    let lastEl = allPages[allPages.length - 1];
+
+    while (!!lastEl?.next_page_url) {
+        allPages = allPages.concat([await axios.get(`${process.env.NEXT_PUBLIC_CORE_SERVICE_ENDPOINT}/sitemaps/jobs?size=1000&page=${lastEl.current_page + 1}`)])
+        lastEl = allPages[allPages.length - 1]
+    }
+
+    const allJobs = allPages.reduce((currentJobs, nextEl) => currentJobs.concat(nextEl.data), [])
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+            <url>
+                <loc>https://senza.vercel.app</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>always</changefreq>
+                <priority>1.0</priority>
+            </url>
+            ${
+                sectors.map(sector => (
+                    `<url>
+                        <loc>https://senza.vercel.app/jobs/${sector.slug}</loc>
+                        <lastmod>${new Date().toISOString()}</lastmod>
+                        <changefreq>hourly</changefreq>
+                        <priority>1.0</priority>
+                    </url>`
+                )).join('')
+            }
+            ${
+                allJobs.map(job => (
+                    `<url>
+                        <loc>https://senza.vercel.app/jobs/${job.slug}</loc>
+                        <lastmod>${new Date(job.updated_at).toISOString()}</lastmod>
+                        <changefreq>daily</changefreq>
+                        <priority>1.0</priority>
+                    </url>`
+                )).join('')
+            }
+        </urlset>
+    `;
+
+    res.setHeader("Content-Type", "text/xml");
+    res.write(sitemap);
+    res.end();
+
+    return {
+        props: {},
+    };
+};
