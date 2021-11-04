5dcd9558347d06c99050aa48b0c2699c6771ece1
Berzel Best <berzel@classifieds.co.zw>
2021-11-04 09:07:43 +0200

    Fix sitemap errors

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -11,7 +11,7 @@ export const getServerSideProps = async ({ req, res }) => {
     const allJobs = allPages.reduce((currentJobs, nextEl) => currentJobs.concat(nextEl.data), [])
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9/ http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
             <url>
                 <loc>${hostname}</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
