7c54897e2c6c5b9336e5177d47ad99bd97c4774b
Berzel Best <berzel@classifieds.co.zw>
2021-11-04 14:48:08 +0200

    Show job modal on click instead of transitioning to a new page

--- components/JobSummary/JobSummary.js
+++ components/JobSummary/JobSummary.js
@@ -1,24 +1,43 @@
 import Link from "next/link";
+import { useState } from "react";
 import JobSummaryStyles from "./JobSummary.styled";
 import JobSummaryDetails from "./JobSummaryDetails";
 
 const JobSummary = ({setActiveJob, job, active}) => {
+    const [showJob, setShowJob] = useState(false);
 
     const updateActiveJob = e => {
+        e.preventDefault();
+
         if (window.innerWidth >= 1024) {
-            e.preventDefault();
             setActiveJob(job);
+        }
+
+        else {
+            setShowJob(true)
         }
     }
 
     return (
-        <JobSummaryStyles active={active}>
-            <Link href={`/job/${job.slug}`} scroll={true}>
-                <a className={`link`} onClick={updateActiveJob}>
-                    <JobSummaryDetails job={job} />
-                </a>
-            </Link>
-        </JobSummaryStyles>
+        <>
+            <JobSummaryStyles active={active}>
+                <Link href={`/job/${job.slug}`} scroll={true}>
+                    <a className={`link`} onClick={updateActiveJob}>
+                        <JobSummaryDetails job={job} />
+                    </a>
+                </Link>
+            </JobSummaryStyles>
+            
+            {
+                showJob && 
+                <div style={{position: 'fixed', top:0, right: 0, left: 0, bottom: 0, backgroundColor: 'white'}}>
+                    {job.title}
+                    <button onClick={e => setShowJob(false)}>
+                        Close
+                    </button>
+                </div>
+            }
+        </>
     )
 }
 
