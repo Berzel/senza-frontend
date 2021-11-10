afa0262c89efa70aa37622d0df63cc7dcbaf1f44
Berzel Best <berzel@classifieds.co.zw>
2021-11-10 12:36:51 +0200

    Close the modal on document click

--- components/NavBar/Menu.js
+++ components/NavBar/Menu.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import tw from "twin.macro"
 import useUser from "../../lib/useUser"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 const MenuStyles = styled.div`
     ${tw`flex items-center space-x-4`}
@@ -43,24 +43,37 @@ const Menu = () => {
     const { user } = useUser();
     const [open, setOpen] = useState(false);
 
+    useEffect(() => {
+        const closeMenu = e => {
+            e.stopPropagation()
+            setOpen(false)
+        }
+
+        document.addEventListener('click', closeMenu)
+
+        return () => {
+            document.removeEventListener('click', closeMenu)
+        }
+    }, [])
+
     return (
         <MenuStyles>
             <div className='_user_btn'>
                 {
                     open ? (
                         // Show the close menu icon
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" onClick={e => setOpen(false)}> 
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" onClick={e => {e.stopPropagation(); setOpen(false)}}> 
                             <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
                         </svg>
                     ) : (
                         user ? (
                             // Show the user icon
-                            <svg style={{transform: 'scale(1.6)'}} viewBox="0 0 24 24" onClick={e => setOpen(!open)}>
+                            <svg style={{transform: 'scale(1.6)'}} viewBox="0 0 24 24" onClick={e => {e.stopPropagation(); setOpen(true)}}>
                                 <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.753 18.305c-.261-.586-.789-.991-1.871-1.241-2.293-.529-4.428-.993-3.393-2.945 3.145-5.942.833-9.119-2.489-9.119-3.388 0-5.644 3.299-2.489 9.119 1.066 1.964-1.148 2.427-3.393 2.945-1.084.25-1.608.658-1.867 1.246-1.405-1.723-2.251-3.919-2.251-6.31 0-5.514 4.486-10 10-10s10 4.486 10 10c0 2.389-.845 4.583-2.247 6.305z"/>
                             </svg>
                         ) : (
                             // Show the hamburger menu icon
-                            <svg  viewBox="0 0 512 512" onClick={e => setOpen(!open)}>
+                            <svg  viewBox="0 0 512 512" onClick={e => {e.stopPropagation(); setOpen(true)}}>
                                 <g>
                                     <path d="m479.18 91.897h-446.36c-18.131 0-32.821-14.69-32.821-32.82s14.69-32.821 32.821-32.821h446.36c18.13 0 32.82 14.69 32.82 32.821s-14.69 32.82-32.82 32.82z"/>
                                     <path d="M295.385,288.821H32.821C14.69,288.821,0,274.13,0,256s14.69-32.821,32.821-32.821h262.564   c18.13,0,32.821,14.69,32.821,32.821S313.515,288.821,295.385,288.821z"/>
