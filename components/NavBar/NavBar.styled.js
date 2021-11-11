454572353fcfc3ba5d223a2072dc8bc15d29eb39
Berzel Best <berzel@classifieds.co.zw>
2021-11-11 08:42:54 +0200

    Show the logged in user email

--- components/NavBar/NavBar.styled.js
+++ components/NavBar/NavBar.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const NavBarStyles = styled.div`
-    ${tw`p-3 bg-gray-800 flex justify-between items-center text-gray-50`}
+    ${tw`p-3 pr-4 bg-gray-800 flex justify-between items-center text-gray-50`}
 
     @media screen and (min-width: 1024px) {
         ${tw`bg-transparent`}
