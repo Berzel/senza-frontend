1cf9762b96e053316b532d19fdbf74fec6e96f1b
Berzel Best <berzel@classifieds.co.zw>
2022-01-06 13:24:57 +0200

    Added privacy policy option to the menu

--- components/NavBar/Menu.js
+++ components/NavBar/Menu.js
@@ -210,6 +210,12 @@ const Menu = () => {
                                     Terms of Use
                                 </span>
                             </li>
+                            <li className="menu_item" onClick={e => e.stopPropagation()}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 7H2v6h8v5l8-8-8-8v5z"/></svg>
+                                <span>
+                                    Privacy Policy
+                                </span>
+                            </li>
                         </ul>
                     </div>
                 </div>
