d95beeced860d8874e3413bdfe0f0c0d17446f14
Brendon Chirume <tchirume14@gmail.com>
2022-01-29 20:01:58 +0200

    WP: aboutpage

---
+++ components/About/TeamSlider.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import SwiperCore, { Navigation } from 'swiper';
+import { Avatar, Box, IconButton, Paper, SvgIcon, Typography } from '@mui/material';
+
+SwiperCore.use([Navigation]);
+
+const TeamSlider = () => {
+  const array = (n) => {
+    if (n < 1) {
+      return [];
+    }
+    const countArray = array(n - 1);
+    countArray.push(n);
+    return countArray;
+  };
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        pl: 3,
+        '& .swiper-slide:first-child': {
+          ml: { xs: '0 !important', md: '16px !important' }
+        }
+      }}
+    >
+      <Swiper
+        slidesPerView={'auto'}
+        spaceBetween={30}
+        navigation
+        breakpoints={{
+          0: {
+            centeredSlides: true
+          },
+          600: {
+            centeredSlides: false
+          },
+          960: {
+            centeredSlides: false
+          }
+        }}
+      >
+        {array(5).map((key) => (
+          <SwiperSlide key={key} style={{ width: 291 }}>
+            <Paper sx={{ borderRadius: 4, p: 2, mb: 3 }}>
+              <Avatar
+                src="https://brendonchirumet.web.app/static/media/profile.8885cbe6.jpg"
+                sx={{ maxWidth: 259, maxHeight: 259, width: '100%', height: '100%' }}
+              />
+
+              <Box textAlign="center" sx={{ mt: 3 }}>
+                <Typography
+                  variant="body1"
+                  sx={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+                >
+                  Brendon Chirume
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ fontWeight: 'bold' }}
+                >
+                  Developer
+                </Typography>
+              </Box>
+              <Box textAlign="center" sx={{ pt: 3 }}>
+                <IconButton>
+                  <SvgIcon
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    role="img"
+                    width="1em"
+                    height="1em"
+                    preserveAspectRatio="xMidYMid meet"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M17 3.5a.5.5 0 0 0-.5-.5H14a4.77 4.77 0 0 0-5 4.5v2.7H6.5a.5.5 0 0 0-.5.5v2.6a.5.5 0 0 0 .5.5H9v6.7a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-6.7h2.62a.5.5 0 0 0 .49-.37l.72-2.6a.5.5 0 0 0-.48-.63H13V7.5a1 1 0 0 1 1-.9h2.5a.5.5 0 0 0 .5-.5z"
+                    ></path>
+                  </SvgIcon>
+                </IconButton>
+                <IconButton>
+                  <SvgIcon
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    role="img"
+                    width="1em"
+                    height="1em"
+                    preserveAspectRatio="xMidYMid meet"
+                    viewBox="0 0 1024 1024"
+                  >
+                    <path
+                      d="M512 378.7c-73.4 0-133.3 59.9-133.3 133.3S438.6 645.3 512 645.3S645.3 585.4 645.3 512S585.4 378.7 512 378.7zM911.8 512c0-55.2.5-109.9-2.6-165c-3.1-64-17.7-120.8-64.5-167.6c-46.9-46.9-103.6-61.4-167.6-64.5c-55.2-3.1-109.9-2.6-165-2.6c-55.2 0-109.9-.5-165 2.6c-64 3.1-120.8 17.7-167.6 64.5C132.6 226.3 118.1 283 115 347c-3.1 55.2-2.6 109.9-2.6 165s-.5 109.9 2.6 165c3.1 64 17.7 120.8 64.5 167.6c46.9 46.9 103.6 61.4 167.6 64.5c55.2 3.1 109.9 2.6 165 2.6c55.2 0 109.9.5 165-2.6c64-3.1 120.8-17.7 167.6-64.5c46.9-46.9 61.4-103.6 64.5-167.6c3.2-55.1 2.6-109.8 2.6-165zM512 717.1c-113.5 0-205.1-91.6-205.1-205.1S398.5 306.9 512 306.9S717.1 398.5 717.1 512S625.5 717.1 512 717.1zm213.5-370.7c-26.5 0-47.9-21.4-47.9-47.9s21.4-47.9 47.9-47.9s47.9 21.4 47.9 47.9a47.84 47.84 0 0 1-47.9 47.9z"
+                      fill="currentColor"
+                    ></path>
+                  </SvgIcon>
+                </IconButton>
+                <IconButton>
+                  <SvgIcon
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    role="img"
+                    width="1em"
+                    height="1em"
+                    preserveAspectRatio="xMidYMid meet"
+                    viewBox="0 0 24 24"
+                  >
+                    <g fill="currentColor">
+                      <path d="M15.15 8.4a5.83 5.83 0 0 0-5.85 5.82v5.88a.9.9 0 0 0 .9.9h2.1a.9.9 0 0 0 .9-.9v-5.88a1.94 1.94 0 0 1 2.15-1.93a2 2 0 0 1 1.75 2v5.81a.9.9 0 0 0 .9.9h2.1a.9.9 0 0 0 .9-.9v-5.88a5.83 5.83 0 0 0-5.85-5.82z"></path>
+                      <rect
+                        x="3"
+                        y="9.3"
+                        width="4.5"
+                        height="11.7"
+                        rx=".9"
+                        ry=".9"
+                      ></rect>
+                      <circle cx="5.25" cy="5.25" r="2.25"></circle>
+                    </g>
+                  </SvgIcon>
+                </IconButton>
+                <IconButton>
+                  <SvgIcon
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    role="img"
+                    width="1em"
+                    height="1em"
+                    preserveAspectRatio="xMidYMid meet"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M8.08 20A11.07 11.07 0 0 0 19.52 9A8.09 8.09 0 0 0 21 6.16a.44.44 0 0 0-.62-.51a1.88 1.88 0 0 1-2.16-.38a3.89 3.89 0 0 0-5.58-.17A4.13 4.13 0 0 0 11.49 9C8.14 9.2 5.84 7.61 4 5.43a.43.43 0 0 0-.75.24a9.68 9.68 0 0 0 4.6 10.05A6.73 6.73 0 0 1 3.38 18a.45.45 0 0 0-.14.84A11 11 0 0 0 8.08 20"
+                    ></path>
+                  </SvgIcon>
+                </IconButton>
+              </Box>
+            </Paper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+};
+export default TeamSlider;
--- pages/about-senza.js
+++ pages/about-senza.js
@@ -3,9 +3,18 @@ import NavBar from '../components/NavBar/NavBar';
 import Banner from '../components/Banner/Banner';
 import Container from '../components/Container/Container';
 import Header from '../components/Header/Header';
-import { Avatar, Box, Grid, Paper, Typography } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  Paper,
+  SvgIcon,
+  Typography
+} from '@mui/material';
+import MuiContainer from '@mui/material/Container';
 import Footer from '../components/Footer/Footer';
-// import TeamSlider from '../components/About/TeamSlider';
+import TeamSlider from '../components/About/TeamSlider';
 
 const AboutSenza = () => (
   <>
@@ -19,13 +28,12 @@ const AboutSenza = () => (
         <NavBar />
         <Banner
           title="Creating Job opotunities to Job Seekers"
-          description=" Together, we&rsquo;re building the future where jobs, locumns and
-              interviews are easily accessible."
+          description="Our vision is to prorate avant Job Board services to the global workforce.."
         />
       </div>
     </Header>
-    <Container sx={{ border: 'none' }}>
-      <Grid container sx={{ pt: { md: 10, xs: 4 } }}>
+    <Container component={MuiContainer} sx={{ border: 'none' }}>
+      <Grid container sx={{ pt: { md: 15, xs: 4 } }}>
         <Grid
           item
           xs={12}
@@ -36,12 +44,10 @@ const AboutSenza = () => (
         >
           <Avatar
             variant="square"
-            src="/img/mission_statement.png"
+            src="/img/missionVecImage.jpg"
             sx={{
-              height: { md: 250 },
-              boxShadow: 10,
-              borderRadius: 4,
-              width: { md: 360 }
+              height: { md: '100%' },
+              width: { md: '80%' }
             }}
           />
         </Grid>
@@ -50,45 +56,68 @@ const AboutSenza = () => (
             Mission
           </Typography>
           <Typography component="p" variant="body1" sx={{ pt: 3 }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci tempora
-            nam eveniet, totam facere a quisquam repudiandae mollitia aperiam eaque,
-            dicta praesentium commodi laboriosam eum! Quam maxime eos quaerat ea.
+            We drive top talent to companies that want to hire them. Our Job Market
+            distributes open jobs from employers to the largest network of recruiters
+            on the planet. Our centralized platform serves as a single point of
+            contact for job seekers, recruiters, and hiring managers. So book your
+            seat right now and get hired. Here we have few special qualities that
+            make us different.
+            <Box component="ul" sx={{ mt: 3 }}>
+              <Box
+                component="li"
+                sx={{ display: 'flex', alignItems: 'center', my: 1 }}
+              >
+                <SvgIcon
+                  sx={{
+                    height: 14,
+                    fill: 'currentcolor',
+                    color: ({ palette }) => palette.primary.main
+                  }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 7H2v6h8v5l8-8-8-8v5z" />
+                </SvgIcon>
+                <span>Efficient</span>
+              </Box>
+              <Box
+                component="li"
+                sx={{ display: 'flex', alignItems: 'center', my: 1 }}
+              >
+                <SvgIcon
+                  sx={{
+                    height: 14,
+                    fill: 'currentcolor',
+                    color: ({ palette }) => palette.primary.main
+                  }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 7H2v6h8v5l8-8-8-8v5z" />
+                </SvgIcon>
+                <span>Free To Join</span>
+              </Box>
+              <Box
+                component="li"
+                sx={{ display: 'flex', alignItems: 'center', my: 1 }}
+              >
+                <SvgIcon
+                  sx={{
+                    height: 14,
+                    fill: 'currentcolor',
+                    color: ({ palette }) => palette.primary.main
+                  }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 7H2v6h8v5l8-8-8-8v5z" />
+                </SvgIcon>
+                <span>Dedicated Support</span>
+              </Box>
+            </Box>
           </Typography>
         </Grid>
       </Grid>
-      <Box
-        sx={{
-          width: '100%',
-          height: { xs: 'fit-content', md: '50vh', lg: '80vh' },
-          mt: 10,
-          borderRadius: 4,
-          position: 'relative',
-          backgroundImage: {
-            xs: '',
-            md: "url('https://minimal-assets-api.vercel.app/assets/images/about/vision.jpg')"
-          },
-          backgroundAttachment: 'fixed',
-          backgroundSize: 'contain'
-        }}
-      >
-        <Typography
-          component="h3"
-          sx={{
-            fontWeight: 'bold',
-            textAlign: { xs: 'center', md: 'left' },
-            fontSize: { xs: '1.75rem', md: '2.3rem' },
-            pt: { xs: 0, md: 10 },
-            width: { xs: '100%', md: 700 },
-            color: { xs: 'initial', md: '#e5e5e5' },
-            position: { xs: 'static', md: 'absolute' },
-            bottom: '5%',
-            left: '3%'
-          }}
-          variant="h4"
-        >
-          Our vision is to prorate avant Job Board services to the global workforce.
-        </Typography>
-      </Box>
       <Typography
         component="h3"
         fontWeight="bold"
@@ -96,23 +125,62 @@ const AboutSenza = () => (
         variant="h4"
         sx={{ pt: 10, pb: 5 }}
       >
-        Values
+        About Us
       </Typography>
-      <Grid container spacing={8} item xs={12} sx={{ py: 3 }}>
-        <Grid item xs={12} md={4}>
-          <Paper
+      <Typography variant="body1" sx={{ pb: 5, px: { md: 5 }, textAlign: 'center' }}>
+        Senza is a leader among marketing and creative staffing agencies. We
+        specialize in connecting talented, creative professionals with companies
+        looking to hire interactive, design, marketing, advertising and public
+        relations talent. We offer flexible staffing solutions to meet companies’
+        project, contract-to-hire and full-time employment needs. <br /> <br />{' '}
+        Through our parent company, we also have the resources to support
+        organizations’ temporary and full-time staffing needs in the areas of
+        technology, accounting, finance, administrative and legal. <br />
+        <br />
+        Throughout our 15 year history we have successfully headhunted across the
+        globe for a range of strategic roles with a 100% success rate. This has been
+        achieved through our dedicated staff, industry experience and high level of
+        quality control throughout every stage of our detailed process. <br />
+        <br />
+        Each individual project we undertake is treated with the highest levels of
+        service and commitment. We pride ourselves on our exemplary work ethic,
+        transparent reporting, accountability and communication – we work 24/7 across
+        all global time zones. <br />
+        <br />
+        We are advisers to the European Parliament on Executive hiring matters. We
+        are also the preferred supplier to several prestigious International VC
+        Companies. Choose us Headhunting as your Executive Search Partner through
+        trust and reputation.
+      </Typography>
+      <Typography
+        component="h3"
+        fontWeight="bold"
+        textAlign="center"
+        variant="h4"
+        sx={{ pt: 10, pb: 5 }}
+      >
+        Our Values
+      </Typography>
+      <Grid container spacing={{ xs: 2, md: 8 }} item xs={12} sx={{ py: 3 }}>
+        <Grid item xs={12} sm={4}>
+          <Box
             sx={{
-              p: 2,
-              boxShadow: 0,
-              borderRadius: 4,
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column'
+              p: { xs: 1, md: 3 },
+              backgroundColor: '#fff'
             }}
           >
-            <Avatar variant="square" sx={{ height: 120, width: 120 }} />
+            <Box
+              sx={{
+                height: 180,
+                margin: '0 auto',
+                width: 180,
+                background: 'url(/img/goldcups.jpg) -18px -15px',
+                backgroundSize: 631,
+                backgroundRepeat: 'no-repeat'
+              }}
+            />
             <Typography
-              sx={{ py: 2 }}
+              sx={{ py: 3 }}
               component="h3"
               textAlign="center"
               fontWeight="bold"
@@ -120,58 +188,176 @@ const AboutSenza = () => (
             >
               Foster sense of security and belonging
             </Typography>
-            <Typography variant="body2" sx={{ px: 3 }}>
+            <Typography variant="body2" sx={{ px: { xs: 1, md: 3 } }}>
               We are committed to achieving fair outcomes for all members of the
               global workforce, beginning with our employees.
             </Typography>
-          </Paper>
+          </Box>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper
+        <Grid item xs={12} sm={4}>
+          <Box
             sx={{
-              p: 2,
-              boxShadow: 0,
-              borderRadius: 4,
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column'
+              p: { xs: 1, md: 3 },
+              backgroundColor: '#fff'
             }}
           >
-            <Avatar variant="square" sx={{ height: 120, width: 120 }} />
-            <Typography sx={{ py: 3 }} component="h3" fontWeight="bold" variant="h6">
-              Reliability
+            <Box
+              src="/img/belonging.jpg"
+              sx={{
+                height: 180,
+                margin: '0 auto',
+                width: 180,
+                background: 'url(/img/goldcups.jpg) -447px -230px',
+                backgroundSize: 631,
+                backgroundRepeat: 'no-repeat'
+              }}
+            />
+            <Typography
+              sx={{ py: 3 }}
+              component="h3"
+              textAlign="center"
+              fontWeight="bold"
+              variant="body1"
+            >
+              Foster sense of security and belonging
             </Typography>
-            <Typography variant="body2" sx={{ px: 3 }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut
-              quasi nulla nobis exercitationem. Nesciunt sint dolores reprehenderit,
+            <Typography variant="body2" sx={{ px: { xs: 1, md: 3 } }}>
+              We are committed to achieving fair outcomes for all members of the
+              global workforce, beginning with our employees.
             </Typography>
-          </Paper>
+          </Box>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper
+        <Grid item xs={12} sm={4}>
+          <Box
             sx={{
-              p: 2,
-              boxShadow: 0,
-              borderRadius: 4,
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column'
+              p: { xs: 1, md: 3 },
+              backgroundColor: '#fff'
             }}
           >
-            <Avatar variant="square" sx={{ height: 120, width: 120 }} />
-            <Typography sx={{ py: 3 }} component="h3" fontWeight="bold" variant="h6">
-              Effectiveness
+            <Box
+              src="/img/belonging.jpg"
+              sx={{
+                height: 180,
+                margin: '0 auto',
+                width: 180,
+                background: 'url(/img/goldcups.jpg) -13px -220px',
+                backgroundSize: 631,
+                backgroundRepeat: 'no-repeat'
+              }}
+            />
+            <Typography
+              sx={{ py: 3 }}
+              component="h3"
+              textAlign="center"
+              fontWeight="bold"
+              variant="body1"
+            >
+              Foster sense of security and belonging
             </Typography>
-            <Typography variant="body2" sx={{ px: 3 }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut
-              quasi nulla nobis exercitationem. Nesciunt sint dolores reprehenderit,
+            <Typography variant="body2" sx={{ px: { xs: 1, md: 3 } }}>
+              We are committed to achieving fair outcomes for all members of the
+              global workforce, beginning with our employees.
             </Typography>
-          </Paper>
+          </Box>
+        </Grid>
+      </Grid>
+      <Typography
+        component="h3"
+        fontWeight="bold"
+        textAlign="center"
+        variant="h4"
+        sx={{ pt: 10, pb: 5 }}
+      >
+        Our Services
+      </Typography>
+      <Grid container spacing={{ xs: 2, md: 4 }} sx={{ py: 3 }}>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              p: { xs: 1, md: 2 },
+              height: 230,
+              backgroundColor: '#fff'
+            }}
+          >
+            <Typography component="h3" fontWeight="bold" variant="h5">
+              Recruiters
+            </Typography>
+            <Divider
+              sx={{
+                mt: 1,
+                mb: 3,
+                width: '30%',
+                borderWidth: 3,
+                borderRadius: 4,
+                borderColor: ({ palette }) => palette.primary.main
+              }}
+            />
+            <Typography>
+              Make more job placements... this is the first place third party
+              recruiters should come to execute your internet strategy. We are the
+              web presence experts for recruiters.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              p: { xs: 1, md: 2 },
+              height: 230,
+              backgroundColor: '#fff'
+            }}
+          >
+            <Typography component="h3" fontWeight="bold" variant="h5">
+              Job Seekers
+            </Typography>
+            <Divider
+              sx={{
+                mt: 1,
+                mb: 3,
+                width: '30%',
+                borderWidth: 3,
+                borderRadius: 4,
+                borderColor: ({ palette }) => palette.primary.main
+              }}
+            />
+            <Typography>
+              Did you know that the best jobs are often never advertised? Recruiters
+              Online Network is is your gateway to this hidden job market.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Box
+            sx={{
+              p: { xs: 1, md: 2 },
+              height: 230,
+              backgroundColor: '#fff'
+            }}
+          >
+            <Typography component="h3" fontWeight="bold" variant="h5">
+              Employers
+            </Typography>
+            <Divider
+              sx={{
+                mt: 1,
+                mb: 3,
+                width: '30%',
+                borderWidth: 3,
+                borderRadius: 4,
+                borderColor: ({ palette }) => palette.primary.main
+              }}
+            />
+            <Typography>
+              Find A Recruiter provides a portal for Recruiters, Headhunters,
+              Executive Search Consultants, Employment Agencies, Staffing Firms &
+              Search Professionals.
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
       <Grid item xs={12} sx={{ pt: 10, pb: 3 }}>
         <Typography component="h3" fontWeight="bold" textAlign="center" variant="h4">
-          Great team is the key
+          Our Team
         </Typography>
 
         <Typography
@@ -190,7 +376,7 @@ const AboutSenza = () => (
         </Typography>
       </Grid>{' '}
       <Grid container spacing={3} item xs={12} sx={{ py: 3 }}>
-        {/* <TeamSlider /> */}
+        <TeamSlider />
       </Grid>
     </Container>
     <Footer />
